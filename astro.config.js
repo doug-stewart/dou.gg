50c7b2e6740274b98f3311d827bbfb4e5c84bf74
Doug Stewart <contact@dougstwart.me>
2024-12-16 04:14:29 -0800

    Set up Sitemap

--- astro.config.js
+++ astro.config.js
@@ -1,14 +1,15 @@
 /* eslint-disable import/named, import/no-unresolved */
 import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
-import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://dou.gg',
     adapter: cloudflare(),
     output: 'server',
-    integrations: [icon(), mdx()],
+    integrations: [icon(), mdx(), sitemap()],
     experimental: {
         responsiveImages: true,
         svg: true,
