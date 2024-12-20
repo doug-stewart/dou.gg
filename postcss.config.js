a30e9150c0987070f0db355e3672ebbdd03183d8
Doug Stewart <contact@dougstwart.me>
2024-12-19 17:04:05 -0800

    remove CSSNano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 import autoprefixer from 'autoprefixer';
-import cssnano from 'cssnano';
 import nesting from 'postcss-nesting';
 
 export default [
     {
-        plugins: [autoprefixer, nesting, cssnano],
+        plugins: [autoprefixer, nesting],
     },
 ];
