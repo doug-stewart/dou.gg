042481d9bafc9778aeb53b767def9415258a5731
Doug Stewart <contact@dougstwart.me>
2024-12-19 16:02:57 -0800

    fixing outstanding linter errors

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,28 @@ import jsxA11y from 'eslint-plugin-jsx-a11y';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 
+const rules = {
+    'import/order': [
+        'error',
+        {
+            groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+            'newlines-between': 'always',
+            alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+    ],
+    'import/default': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-unresolved': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+};
+
 export default tseslint.config(
     js.configs.recommended,
     tseslint.configs.recommended,
@@ -15,46 +37,17 @@ export default tseslint.config(
     importPlugin.flatConfigs.recommended,
     eslintPluginAstro.configs['flat/recommended'],
     {
-        files: ['**/*.astro', '**/*.{ts,tsx}', '**/*.{js,jsx}'],
+        files: ['**/*.{astro,d.ts,js,jsx,ts,tsx}'],
         plugins: {
             'jsx-a11y': jsxA11y,
             '@typescript-eslint': tsESlintPlugin,
         },
         languageOptions: {
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parserOptions: { ecmaFeatures: { jsx: true } },
         },
-        rules: {
-            'import/order': [
-                'error',
-                {
-                    groups: [
-                        'builtin',
-                        'external',
-                        'internal',
-                        'parent',
-                        'sibling',
-                        'index',
-                        'object',
-                    ],
-                    'newlines-between': 'always',
-                    alphabetize: { order: 'asc', caseInsensitive: true },
-                },
-            ],
-            'import/default': 'off',
-            'import/no-named-as-default-member': 'off',
-            'import/no-named-as-default': 'off',
-            'jsx-a11y/anchor-is-valid': 'off',
-            '@typescript-eslint/no-unused-vars': 'error',
-            '@typescript-eslint/explicit-function-return-type': 'off',
-            '@typescript-eslint/explicit-module-boundary-types': 'off',
-            '@typescript-eslint/no-empty-function': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
-            'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-        },
+        rules,
     },
     {
         files: ['**/*.astro'],
@@ -66,13 +59,6 @@ export default tseslint.config(
         },
     },
     {
-        ignores: [
-            '.github/*',
-            '.astro/*',
-            'dist/*',
-            'node_modules/*',
-            '**/env.d.ts',
-            'types.generated.d.ts',
-        ],
+        ignores: ['.github/*', '.astro/*', 'dist/*', 'node_modules/*', 'types.generated.d.ts'],
     },
 );
