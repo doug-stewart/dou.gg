c94d6bc8d5642f1cabf47b023b76e9808c460fd0
Doug Stewart <hi@dou.gg>
2025-01-04 21:41:19 -0800

    Transition to Cloudflare deploys

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,10 @@
-//eslint-disable-next-line
+/* eslint-disable import/named, import/no-unresolved */
+import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dou.gg',
+    adapter: cloudflare(),
+    output: 'server',
 });
